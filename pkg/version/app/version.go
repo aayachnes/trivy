b661c538f8eca43fa31c2f7b48586623d4aea2ba
Ali Yachnes <ali@arnica.io>
2025-07-09 14:08:34 -0400

    Update tag to v0.56.2-maven-patch-0.0.1

--- pkg/version/app/version.go
+++ pkg/version/app/version.go
@@ -2,7 +2,7 @@ package app
 
 // For some reason, the version is not being injected from git tags through goreleaser
 // The version is hardcoded here for now
-var ver = "v0.56.2-maven-patch"
+var ver = "v0.56.2-maven-patch-0.0.1"
 
 func Version() string {
 	return ver
